c43d58e756f497540d7d94c7ce62ac95e6495bfb
mshanemc <shane.mclaughlin@salesforce.com>
2020-08-28 13:04:45 -0500

    move git config and env to profile.d

---
+++ profile.d/github-via-pat.sh
@@ -0,0 +1,11 @@
+# echo 'echo $GITHUB_PAT' > $HOME/.git-askpass
+# chmod +x $HOME/.git-askpass
+# export GIT_ASKPASS=$HOME/.git-askpass
+
+echo "buildpack github-via-pat: modifying git config"
+git config --global url."https://api@github.com/".insteadOf "https://github.com/"
+git config --global url."https://ssh@github.com/".insteadOf "ssh://git@github.com/"
+git config --global url."https://git@github.com/".insteadOf "git@github.com:"
+
+export GIT_ASKPASS=$HOME/.git-askpass
+
--- profile/github-via-pat.sh
+++
@@ -1,3 +0,0 @@
-# echo 'echo $GITHUB_PAT' > $HOME/.git-askpass
-# chmod +x $HOME/.git-askpass
-# export GIT_ASKPASS=$HOME/.git-askpass
