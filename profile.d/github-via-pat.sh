2f2bb171daf0005fb6514e94b47ec5b2b70c9398
mshanemc <shane.mclaughlin@salesforce.com>
2020-08-28 13:22:06 -0500

    quotes syntax

--- profile.d/github-via-pat.sh
+++ profile.d/github-via-pat.sh
@@ -7,5 +7,5 @@ git config --global url."https://api@github.com/".insteadOf "https://github.com/
 git config --global url."https://ssh@github.com/".insteadOf "ssh://git@github.com/"
 git config --global url."https://git@github.com/".insteadOf "git@github.com:"
 
-export GIT_ASKPASS=$HOME/.git-askpass
+export "GIT_ASKPASS=$HOME/.git-askpass"
 
